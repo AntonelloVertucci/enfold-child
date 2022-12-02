997db5c9753e124a5eae43b6f6b3e24a294b72b1
AntonelloVertucci <52776708+AntonelloVertucci@users.noreply.github.com>
2022-12-02 17:55:51 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -5,14 +5,14 @@
     Backend Footer
     =============================================
 */
-function NM_footer_admin_left_func(){
+function AV_footer_admin_left_func(){
     $blog_public = 0 == get_option( 'blog_public' ) ? '<span style="color: red">OFF</span>' : '<span style="color: green">ON</span>';
     echo '<span style="font-size: 11px; color:black; letter-spacing: 2px;">NECKARMEDIA</span>
           <span style="font-size: 11px; color:red;">PHP ' . PHP_VERSION . '</span> |  
           <span style="font-size: 11px; color:blue;">WP ' . get_bloginfo( "version" ) .'</span> | 
           <span style="font-size: 11px; color:black;">SEO-Index:</span> <b>' . $blog_public . '</b>';
 }
-add_filter('admin_footer_text', 'NM_footer_admin_left_func');
+add_filter('admin_footer_text', 'AV_footer_admin_left_func');
 
 
 /*
@@ -21,48 +21,48 @@ add_filter('admin_footer_text', 'NM_footer_admin_left_func');
     =============================================
 */
 /* Debug modus */
-function NM_enfold_debug_modus_func(){
+function AV_enfold_debug_modus_func(){
   return "debug";
 }
-add_action('avia_builder_mode', "NM_enfold_debug_modus_func");
+add_action('avia_builder_mode', "AV_enfold_debug_modus_func");
 
 /* Remove Avia Framework debug information */
 if(!function_exists('avia_debugging_info')){
-  	function avia_debugging_info() {}
+  	function avia_debugging_info(){}
 }
 
 /* Remove the Import Dummy Data button (Demo) */
 add_theme_support('avia_disable_dummy_import');
 
 /* Disable Portfolio 
-function NM_disable_portfolio_func(){
+function AV_disable_portfolio_func(){
     remove_action('init', 'portfolio_register');
 }
-add_action('after_setup_theme', 'NM_disable_portfolio_func');
+add_action('after_setup_theme', 'AV_disable_portfolio_func');
 */
 
 /* Replace portfolio name */
-function NM_change_portfolio_name_func($args){
+function AV_change_portfolio_name_func($args){
     $args['labels']['name'] = 'Produkte';
     return $args;
 }
-add_filter('avf_portfolio_cpt_args', 'NM_change_portfolio_name_func');
+add_filter('avf_portfolio_cpt_args', 'AV_change_portfolio_name_func');
 
 /* Hide editor info / Post state */
-function NM_remove_ALB_post_state_func( $post_states, $post ){
+function AV_remove_ALB_post_state_func( $post_states, $post ){
     if("! has_blocks( $post->ID )") {
         unset($post_states['wp_editor']);
     }
-    if("!= Avia_Builder()->get_alb_builder_status($post->ID)") {
+    if("!= Avia_Builder()->get_alb_builder_status($post->ID)"){
         unset($post_states['avia_alb']);
     }
 return $post_states;
 }
-add_filter('display_post_states','NM_remove_ALB_post_state_func',999,2);
+add_filter('display_post_states','AV_remove_ALB_post_state_func', 999, 2);
 
 /* Remove Enfold Image sizes */
-function NM_remove_enfold_image_sizes_func() {
-//  remove_image_size('widget');
+function AV_remove_enfold_image_sizes_func(){
+    //remove_image_size('widget');
 	remove_image_size('square');
 	remove_image_size('featured');
 	remove_image_size('featured_large');
@@ -78,7 +78,7 @@ function NM_remove_enfold_image_sizes_func() {
 	remove_image_size('shop_single'); 
   remove_image_size('shop_gallery_thumbnail');
 }
-add_action('init', 'NM_remove_enfold_image_sizes_func');
+add_action('init', 'AV_remove_enfold_image_sizes_func');
 
 
 /*
@@ -86,45 +86,13 @@ add_action('init', 'NM_remove_enfold_image_sizes_func');
     Hide Updates notification in WP Backend
     =============================================
 */
-function NM_hide_update_notification_func() {
+function AV_hide_update_notification_func(){
   echo '<style>
         .update-nag,
         .update-plugins{display: none !important}
         </style>';
 }
-add_action('admin_head', 'NM_hide_update_notification_func');
-
-
-/*
-    =============================================
-    Add Category in Body Class if is single
-    =============================================
-*/
-function NM_body_class_add_categories_func( $classes ){
-    if ( !is_single() ) return $classes;
-
-    $post_categories = get_the_category();
-    foreach( $post_categories as $current_category ) {
-        $classes[] = 'cat-' . $current_category->slug;
-    }
-    return $classes;
-}
-add_filter( 'body_class', 'NM_body_class_add_categories_func' );
-
-
-/*
-    =============================================
-    Login error
-    =============================================
-*/
-function NM_login_errors_func($error){
-    $pos = strpos($error, 'incorrect');
-    if (is_int($pos)){
-        $error = "Error...";
-    }
-    return $error;
-}
-add_filter('login_errors', 'NM_login_errors_func');
+add_action('admin_head', 'AV_hide_update_notification_func');
 
 
 /*
@@ -132,7 +100,7 @@ add_filter('login_errors', 'NM_login_errors_func');
     Login Logo
     =============================================
 */
-function NM_custom_login_logo_func(){
+function AV_custom_login_logo_func(){
     echo '<style type="text/css">
         h1 a { background-image:url('. get_stylesheet_directory_uri() .'/img/loginlogo.png) !important; 
         background-size:250px 100px !important;
@@ -141,7 +109,7 @@ function NM_custom_login_logo_func(){
         }
     </style>';
 }
-add_action('login_head', 'NM_custom_login_logo_func');
+add_action('login_head', 'AV_custom_login_logo_func');
 
 
 /*
@@ -149,7 +117,7 @@ add_action('login_head', 'NM_custom_login_logo_func');
     Remove Wordpress Header Info
     =============================================
 */
-function NM_remove_header_info_func(){ 
+function AV_remove_header_info_func(){ 
     remove_action('wp_head', 'feed_links_extra', 3); 
     remove_action('wp_head', 'rsd_link'); 
     remove_action('wp_head', 'wlwmanifest_link'); 
@@ -159,7 +127,7 @@ function NM_remove_header_info_func(){
     remove_action('wp_head', 'parent_post_rel_link', 10, 0); 
     remove_action('wp_head', 'adjacent_posts_rel_link_wp_head',10,0); 
 } 
-add_action('init', 'NM_remove_header_info_func');
+add_action('init', 'AV_remove_header_info_func');
 
 
 /*
@@ -176,13 +144,11 @@ add_action( 'after_setup_theme', function(){
 
 /*
     =============================================
-    Enqueue script
+    Enqueue scripts
     =============================================
-
-function NM_enqueue_script_func() {    
-    wp_register_script('script', get_stylesheet_directory_uri() . '/js/script.js', array('jquery'),'', true);
+*/
+function AV_enqueue_scripts_func(){    
+    wp_register_script('script', get_stylesheet_directory_uri() . '/js/script.js','','', true);
     wp_enqueue_script('script');
 }
-add_action('wp_enqueue_scripts', 'NM_enqueue_script_func');
-*/
-
+add_action('wp_enqueue_scripts', 'AV_enqueue_scripts_func');
