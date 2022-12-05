6d96e3fc845f92fb9501f6cd53f10b69e0c51276
AntonelloVertucci <52776708+AntonelloVertucci@users.noreply.github.com>
2022-12-05 12:18:43 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -73,7 +73,10 @@ add_action('init', 'AV_remove_enfold_image_sizes_func');
     =============================================
 */
 function AV_footer_admin_left_func(){
-    $blog_public = 0 == get_option( 'blog_public' ) ? '<span style="color: red">OFF</span>' : '<span style="color: green">ON</span>';
+    $blog_public = 0 == 
+    get_option( 'blog_public' ) 
+    ? '<span style="color: red">OFF</span>' 
+    : '<span style="color: #00b400; font-size: 10px; padding: 0.2em 0.4em; background: rgb(0 255 0 / 10%); border-radius: 4px">ON</span>';
     echo '<span style="color: black; font-family: Helvetica, sans-serif; font-weight: 700; margin: 0 .3em 0 0">neckarmedia</span>
           <span style="font-size: 11px; color:red;">PHP ' . PHP_VERSION . '</span> |  
           <span style="font-size: 11px; color:blue;">WP ' . get_bloginfo( "version" ) .'</span> | 
