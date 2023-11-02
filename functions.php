757357341b3c201c3218d9405ee922ea69db0436
AntonelloVertucci <52776708+AntonelloVertucci@users.noreply.github.com>
2023-11-02 10:48:23 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -187,10 +187,3 @@ function AV_enqueue_scripts_func(){
 }
 add_action('wp_enqueue_scripts', 'AV_enqueue_scripts_func');
 
-
-/*
-    =============================================
-    Remove <p> and <br/> from Contact Form 7
-    =============================================
-*/
-add_filter('wpcf7_autop_or_not', '__return_false');
